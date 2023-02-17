84f0b13c3c6e6e5131b2afc57b8df0cd51e6dd8e
olyalyutaya <you@example.com>
2023-02-17 12:38:54 +0300

    мщй супер коммит

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,18 @@
-"use strict"
-function solveEquation(a, b, c) {
+"use strict";
+
+function solveEquation(a, b ,c) {
   let arr = [];
-  
+  let d = b ** 2 - 4 * a * c;
+
+  if (d > 0) {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x1, x2);
+  }
+  else if (d === 0) {
+    let x3 = d - b/(2*a);
+    arr.push(x3);
+}
   return arr;
 }
-
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+console.log(solveEquation(1, 12, 36));
