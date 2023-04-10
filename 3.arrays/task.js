7bb060c40d407063aa77c7f9d236f92bd7d6d54a
olyalyutaya <you@example.com>
2023-04-10 22:07:08 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,22 @@
 function compareArrays(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+      return false; // массивы имеют разную длину
+    }
+    return arr1.every(function(value, index) {
+      return value === arr2[index];
+    });
+  }
+  function getUsersNamesInAgeRange(users, gender) {
+    let filteredUsers = users.filter(function(user) {
+      return user.gender === gender;
+    });
+    let ages = filteredUsers.map(function(user) {
+      return user.age;
+    });
+    let sum = ages.reduce(function(acc, value) {
+      return acc + value;
+    }, 0);
+    let average = sum / ages.length;
+    return average;
+  }
   
-}
-
-function getUsersNamesInAgeRange(users, gender) {
-  
-}
